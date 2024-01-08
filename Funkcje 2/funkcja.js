2fd9c8272e68a357840b21d6ced822db6b2fcf31
Oleg Szcxesniak <109421550+olegszczesniak@users.noreply.github.com>
2024-01-08 12:59:33 +0100

    Add files via upload

---
+++ Funkcje 2/funkcja.js
@@ -0,0 +1,42 @@
+/*
+document.write(parseInt("123"));
+
+document.write(parseInt("123AB", 16 ));
+
+document.write(parseInt("123" , 8));
+
+document.write(parseInt("123AB", 8));
+
+document.write(parseInt("0377"));
+
+document.write(parseInt("0x373"));
+
+
+let c1 = "12";
+let c2 = "34";
+let sum1 = c1+c2;
+
+document.write ("Suma" + sum1 + "<br>");
+
+c1 = parseInt (c1);
+c2 = parseInt (c2);
+let sum2 =  c1+c2;
+
+document.write ("Suma: " + sum2 + "<br>");
+
+
+document.write(isNaN (parseInt("zx23")));
+document.write(isNaN(NaN));
+document.write(isNaN(567));
+document.write(isNaN(37.2));
+
+
+document.write (isFinite (Infinity))
+document.write (isFinite (-Infinity))
+document.write (isFinite (67))
+document.write (isFinite (2E12))
+*/
+
+
+
+
